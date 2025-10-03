a8759a2ead2e6dee6ad050c6e8e96d4b00fcfaa8
Matthew F. McEneaney <mfm45@duke.edu>
2025-10-03 12:01:41 -0400

    fix: Add logger statements and fix type name in deploy module.

--- core/lambdaml/deploy.py
+++ core/lambdaml/deploy.py
@@ -16,7 +16,7 @@ from .log import setup_logger
 logger = setup_logger(__name__)
 
 
-class TITOKModelWrapper:
+class ModelWrapper:
     def __init__(
         self,
         encoder_type=FlexibleGNNEncoder,
@@ -29,33 +29,42 @@ class TITOKModelWrapper:
     ):
 
         # Load encoder
+        logger.debug("Loading encoder parameters from %s", encoder_params_path)
         encoder_params = {}
         with open(encoder_params_path, "r", encoding="utf-8") as f:
             encoder_params = json.load(f)
+        logger.debug("Creating encoder from encoder_params = %s", encoder_params)
         self.encoder = encoder_type(**encoder_params)
+        logger.debug("Loading encoder state dictionary to %s from %s", map_location, encoder_path)
         self.encoder.load_state_dict(
             torch.load(encoder_path, map_location=map_location)
         )
         self.encoder.eval()
 
         # Load classifier
+        logger.debug("Loading classifier parameters from %s", clf_params_path)
         clf_params = {}
         with open(clf_params_path, "r", encoding="utf-8") as f:
             clf_params = json.load(f)
+        logger.debug("Creating classifier from clf_params = %s", clf_params)
         self.clf = clf_type(**clf_params)
+        logger.debug("Loading classifier state dictionary to %s from %s", map_location, clf_path)
         self.clf.load_state_dict(torch.load(clf_path, map_location=map_location))
         self.clf.eval()
 
     def predict(self, graph_json):
 
         # Assume you are passing a json or dictionary object
+        logger.debug("Loading data from graph_json[\"x\",\"edge_index\"]")
         x = torch.tensor(graph_json["x"], dtype=torch.float)
         edge_index = torch.tensor(graph_json["edge_index"], dtype=torch.long)
 
         # Create the graph
+        logger.debug("Creating graph from x = %s\n\tand edge_index = %s", x, edge_index)
         data = Data(x=x, edge_index=edge_index)
 
         # Run the graph through the model
+        logger.debug("Applying encoder, classifier, softmax")
         with torch.no_grad():
             feat = self.encoder(data)
             logit = self.clf(feat)
@@ -84,9 +93,9 @@ def select_best_model(
     best_trial = study.best_trial
 
     # Access hyperparameters and custom user attributes
-    print("Best trial value:", best_trial.value)
-    print("Hyperparameters:", best_trial.params)
-    print("Trial user attributes:", best_trial.user_attrs)
+    logger.info("Best trial value: %s", best_trial.value)
+    logger.info("Hyperparameters: %s", best_trial.params)
+    logger.info("Trial user attributes: %s", best_trial.user_attrs)
 
     # Load encoder and classifier file paths (assuming you stored them with trial.set_user_attr)
     encoder_path = best_trial.user_attrs["encoder_path"]
@@ -95,12 +104,16 @@ def select_best_model(
     clf_params_path = best_trial.user_attrs["clf_params_path"]
 
     # Copy models and params to gnn server directory
+    logger.debug("Copying encoder:\n\t%s\n\t -> %s", encoder_path, osp.join(gnn_server_dir, encoder_fname))
     shutil.copy(encoder_path, osp.join(gnn_server_dir, encoder_fname))
+    logger.debug("Copying encoder params:\n\t%s\n\t -> %s", encoder_params_path, osp.join(gnn_server_dir, osp.basename(encoder_params_fname)))
     shutil.copy(
         encoder_params_path,
         osp.join(gnn_server_dir, osp.basename(encoder_params_fname)),
     )
+    logger.debug("Copying classifier:\n\t%s\n\t -> %s", clf_path, osp.join(gnn_server_dir, clf_fname))
     shutil.copy(clf_path, osp.join(gnn_server_dir, osp.basename(clf_fname)))
+    logger.debug("Copying classifier params:\n\t%s\n\t -> %s", clf_params_path, osp.join(gnn_server_dir, osp.basename(clf_params_fname)))
     shutil.copy(
         clf_params_path, osp.join(gnn_server_dir, osp.basename(clf_params_fname))
     )
